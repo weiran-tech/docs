f49d3c29fc1b61470365930d6f64326cc90ae3a1
duoli <zhaody901@126.com>
2025-02-07 23:11:00 +0800

    auto commit @2025-02-07 23:11:00

--- .vitepress/nav.ts
+++ .vitepress/nav.ts
@@ -1,4 +1,42 @@
 export default {
+    "/wr-1.x/": [
+        {
+            "text": "Poppy Framework 10.x",
+            "link": "/wr-1.x/index"
+        },
+        {
+            "text": "Poppy Framework 4.x",
+            "link": "/4.x/index"
+        },
+        {
+            "text": "Poppy Framework",
+            "link": "/3.x/index"
+        },
+        {
+            "text": "升级说明",
+            "link": "/10.x/upgrade"
+        },
+        {
+            "text": "升级说明",
+            "link": "/4.x/upgrade"
+        },
+        {
+            "text": "常见问题",
+            "link": "/3.x/faq"
+        },
+        {
+            "text": "FAQ",
+            "link": "/10.x/faq"
+        },
+        {
+            "text": "FAQ",
+            "link": "/4.x/faq"
+        },
+        {
+            "text": "开发计划",
+            "link": "/3.x/framework/plan"
+        }
+    ],
     "/10.x/": [
         {
             "text": "支付宝支付",
@@ -64,7 +102,7 @@ export default {
         },
         {
             "text": "最佳实践",
-            "link": "/10.x/project/best-practice"
+            "link": "/wr-1.x/project/best-practice"
         },
         {
             "text": "说明",
